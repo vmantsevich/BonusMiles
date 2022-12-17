d2a8719b9d30aee7b6c3e292830e84315d391b83
VMantsevich <vladislava_mantsevich@mail.ru>
2022-12-17 20:03:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2450;
+        int bonusMiles = 20;
+        System.out.println("Начисленные балы: " + price / bonusMiles);
+    }
+}
